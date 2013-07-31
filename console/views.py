76a84b3748438e27b8125b5d94a4647bb21ca2ae
Mark Aufdencamp <Mark@Aufdencamp.com>
2013-07-31 14:47:34 -0400

    Fixed video for landing page

--- console/views.py
+++ console/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<!DOCTYPE html><html lang="en"><head><title>su SEO Console</title></head><body><h1>su SEO Console</h1><video srv="http://suseo.us/suSEOus.mov"></video></body></html>')
+    return HttpResponse('<!DOCTYPE html><html lang="en"><head><title>su SEO Console</title></head><body><h1>su SEO Console</h1><video src="http://suseo.us/suSEOus.mov" autoplay width="75%" height="75%"></video></body></html>')
